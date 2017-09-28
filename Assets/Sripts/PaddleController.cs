b87f508cc2e45b9c11bf24259587686cf448ad67
Barry <barry.p.evans@gmail.com>
2017-09-28 13:56:11 -0700

    [+] git commit

---
+++ Assets/Sripts/PaddleController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    Camera camera;
+
+    // Use this for initialization
+    void Start()
+    {
+        camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = camera.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = new Vector3(newPosition.x, transform.position.y, transform.position.z);
+
+    }
+}
