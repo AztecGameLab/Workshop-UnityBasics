db8ca02dd3145c90489d95bf39323b80bf8bbafa
Barry <barry.p.evans@gmail.com>
2017-10-02 13:53:43 -0700

    [+] added bouncing ball and physics

---
+++ Assets/Sripts/BallController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        Vector2 randomVelocity = new Vector2(Random.Range(0f, 1f), Random.Range(0f, 1f));
+        randomVelocity.Normalize();
+        rigidbody.velocity = randomVelocity * 10;
+    }
+	
+}
